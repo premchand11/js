5d8e294f979acad238e62d8653750ed0a879d851
Premchand Panku <pankupremchand@gmail.com>
2025-01-24 06:15:19 +0000

    Changes to be committed: 	modified:   .devcontainer/devcontainer.json 	new file:   01_basics/datatypes.js 	new file:   01_basics/varibles.js

---
+++ 01_basics/varibles.js
@@ -0,0 +1,14 @@
+const accID = 14453
+let accountemail ="pankupremchand@gmail.com"
+var pssword="pass"
+accountcity="hyd"
+let accountstate
+
+// accID=2
+// we dont use var mostly because of scope(block and functional)
+
+accountcity="blr"
+accountemail="chand@gmail.com"
+console.table([accID,accountemail,accountcity,accountstate])
+
+
