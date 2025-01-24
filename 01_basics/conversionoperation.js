df435aed9e2b368adfcea0e082204f9ce2d9783a
Premchand Panku <pankupremchand@gmail.com>
2025-01-24 06:43:46 +0000

    conversion of datatypes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,26 @@
+// let score="hello"
+// console.log(typeof(score))
+// console.log(typeof score)
+// let valueinNUmber=Number(score)
+// console.log(typeof valueinNUmber)
+// console.log(valueinNUmber)
+
+
+ //33=>33
+ //33abc=>NaN
+ //abc=>NaN
+ //null=>0
+ //undefined=>NaN
+ // true=>1/false=>0
+
+
+let isloggedin=1;
+let booleanisloggedin=Boolean(isloggedin);
+console.log(isloggedin)
+console.log(booleanisloggedin)
+
+
+// 1=>true
+// 0=>false
+// hi=>true
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // newer version(treat all code)
+// alert(3+3) we are using node js not browser
+
+console.log(3+3);console.log("hi") // code readabilty is not good
+
+let name="prem"
+let num=7
+let isloggedin = false
+
+// number=2 power 53
+// string=""
+//boolean => true or false
+//null=> standalone value
+//undefined=>
+//symbol => uniquness
+
+
+//object=>
+
+console.log(typeof name);
