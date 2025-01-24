a11a853e83b4245d066306a0037e4fd018477241
Premchand Panku <pankupremchand@gmail.com>
2025-01-24 09:01:44 +0000

    conversion and comparison

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -14,13 +14,49 @@
  // true=>1/false=>0
 
 
-let isloggedin=1;
-let booleanisloggedin=Boolean(isloggedin);
-console.log(isloggedin)
-console.log(booleanisloggedin)
+// let isloggedin=1;
+// let booleanisloggedin=Boolean(isloggedin);
+// console.log(isloggedin)
+// console.log(booleanisloggedin)
 
 
 // 1=>true
 // 0=>false
 // hi=>true
+
+// ************opeartions*************
+
+
+let value=6
+let negvalue=-value
+console.log(negvalue)
+// +,-,*,**,/,%
+
+let str1="hi"
+let str2=" hello"
+let str3=str1+str2
+console.log(str3)
+
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
 
